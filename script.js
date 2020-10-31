0813c0d5a21be7e880a4ac638b83176616138075
Zach Taylor <ztayut2tx@gmail.com>
2020-10-31 16:04:02 -0500

    added arrays and declared variables

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-var generate Btn = document.querySelector("#generate");
+// Code
+var generateBtn = document.querySelector("#generate");
+
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*";
+var charSet = "";
+
+var confirmLength = "";
+var confirmsymbols;
+var confirmnumbers;
+var confirmlowCase;
+var confirmupCase;
+
+
+
+
+
+}
