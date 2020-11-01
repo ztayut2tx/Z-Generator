2867bef7c8edb82ed9bece2d41ec8818e0e36a94
Zach Taylor <ztayut2tx@gmail.com>
2020-10-31 20:35:39 -0500

    commented out js doc, filled out readme, final commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Z-Generator
-Re do of generator z
+Re do of generator z (first go was definitely not right)
+
+# Description
+This is a random password generator that functions based off the users selections they make to certain criterias that they want their passwords to meet.  It is also my first real dive into javascript and let me tell ya, what a headache its been.  I am very pleased to say that this generator is functioning to the best of my knowledge and I am quite a bit more fond of javascript now that i made it through this first one.
+
+# Usage
+Use this application to generate a much more secure random password.
+
+# Roadmap
+Thats probably it for this particular project.  It was a great challenging introduction to JS.
+
+# Acknowledgements
+My Teachers, peers, tutors and loved ones (for keeping me sane through it). W3schools.com, google.com, random github and google found pages where i was able to refresh my memory on ifs,else,else if, find solutions to problems and utilize commands im not super familar with.
+
+# Project Status
+COMPLETE
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// Code
+// proclaimed arrays
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var symbols = ["!", "@", "#", "$", "%", "&"];
 var getBig = "";
 var space = [];
-
+// proclaimed confirms
 var Qa
 var confirmSymbols;
 var confirmNumbers;
 var confirmLowCase;
 var confirmUpCase;
-
+// variable for users selections from prompts
 var choices;
-
+// command to change alphabet array to upper case.
 var toUpper = alphabet.map(alphabet => alphabet.toUpperCase());
      
 
     
 
 
-
+//variable for upper case alphabet
 var alpha = toUpper;
-
+//button reference
 var GeeBtn = document.querySelector("#generate");
 
 GeeBtn.addEventListener("click", function () {
     gp = generateP();
     document.getElementById("password").placeholder = gp;
 });
-
+//function to generate random numbers based on user choices
 function generateP() {
-
+//initial prompt after clicking button
     Qa = parseInt(prompt("Your new password can be between 8 and 128 characters along, please enter a number"));
-
+//prompt if proper value wasnt inputed
     if (!Qa) {
         alert("This needs a Number");
     }
@@ -41,7 +41,7 @@ function generateP() {
 
         Qa = parseInt(prompt("Choose beetween 8 and 128"));
     }
-    
+// password criteria section which allows us to get the users choices
     else {
          confirmSymbols = confirm("Will this password contain symbols?");
          confirmNumbers = confirm("Will this password contain number?");
@@ -105,20 +105,21 @@ function generateP() {
     else if (confirmUpCase) {
         choices = space.concat(alpha);
     };
-
+//this represents where the generated password will be stored.
     var password = [];
-
+//the equation to generate random numbers, takes the user choices and does random math based on the items in the length of all arrays selected.
     for (var i = 0; i < Qa; i++){
         var picks = choices[Math.floor(Math.random()*choices.length)];
         password.push(picks);
     }
 
     var gp = password.join("");
+    
     UserInput(gp);
     
     return gp;
 };
-
+// sends new password to display box.
 function UserInput(gp) {
     document.getElementById("password").textContent = gp;
 }
